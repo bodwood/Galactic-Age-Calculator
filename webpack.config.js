42908eb0d28d571c51f5d8bd1da38c94780dbee7
bodwood <bodiewood@hotmail.com>
2022-11-14 09:22:00 -0800

    add initial files for project. Update project name in package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'UPDATE NAME HERE',
+      title: 'Galactic-Age-Calculator',
       template: './src/index.html',
       inject: 'body'
     })
